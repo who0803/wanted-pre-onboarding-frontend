50f07fd7e88967c4a173de47b2c6223b7b9cdba7
who0803 <10rlatkdtn@naver.com>
2023-06-21 23:11:15 +0900

    feat: Remove strictmode
    - 개발모드에서 useEffect가 두번 실행되기 때문에 제거

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import router from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
